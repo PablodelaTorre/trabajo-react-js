8d915b6a0b14e821c8c1a45f01e5302b8d08de06
Pablo <delatorre.pablodaniel@gmail.com>
2022-02-18 01:10:24 -0300

    Desafio terminado

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,6 +4,14 @@ const NavBar = () => {
     return (
     <>
         
+        <nav className='listaNav'>
+            <h1>Ecommerce</h1>
+            <ul className=''>
+                <li><a href="">Home</a></li>
+                <li><a href="">Productos</a></li>
+                <li><a href="">Contacto</a></li>
+            </ul>
+        </nav>
     </>
     )
 }
