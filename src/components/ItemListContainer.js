0292b7ed8a9b38eecbc4e0de6c8aedb61add8155
Pablo <delatorre.pablodaniel@gmail.com>
2022-02-25 22:55:19 -0300

    ItemCount terminado menos el onAdd

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import ItemCount from './ItemCount'
+
 
 const ItemListContainer = ({greeting}) => {
     return (
-        <div className='fs-2 fw-bold d-flex justify-content-center itemListContainer my-4 '>{greeting}</div>
+        <>
+            <div className='fs-2 fw-bold d-flex justify-content-center itemListContainer my-4 '>{greeting}</div>
+            <ItemCount stock={6} initial={1} onAdd/>
+        </>
     )
 }
 
