20399ca745a9a4027012e2397f6f31e94dcfd10f
Pablo <delatorre.pablodaniel@gmail.com>
2022-03-10 10:46:51 -0300

    Tarjetas una al lado de la otra

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -4,20 +4,18 @@ import Item from './Item'
 const ItemList = ({items}) => {
     return (
         <>
-            {items.map(item => {
-                return(
-                    <>
-                        <div className='container-fluid'>
-                            <div className='row d-flex'>
-                                <div className='col-sm-3'>
-                                    <Item key={item.id} item={item}/>
-                                </div>
-                            </div>
-                        </div>
-                    </>
-                )
-                })
-            }
+            <section className='render-prod'>
+                    {items.map(item => {
+                        return(
+                            <>
+                                
+                                <Item key={item.id} item={item}/>
+                                        
+                            </>
+                        )
+                        })
+                    }
+            </section>
         </>
     )
 }
