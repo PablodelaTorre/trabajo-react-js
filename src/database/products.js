54953244667649c5183059cfaaff2db7ec240dc9
Pablo <delatorre.pablodaniel@gmail.com>
2022-03-11 23:19:23 -0300

    imagenes items agregadas

--- src/database/products.js
+++ src/database/products.js
@@ -4,7 +4,7 @@ const products = [{
     "description": "Clos bx saliv gland/duct",
     "price": 49354.36,
     "category": "Murray and Sons",
-    "imageUrl": "https://dummyimage.com/250x250/0e7fe1/fff",
+    "imageUrl": "https://picsum.photos/200/100/",
     "stock": 487
   }, {
     "id": 2,
@@ -12,7 +12,7 @@ const products = [{
     "description": "Culture-integument",
     "price": 61077.33,
     "category": "Conn Group",
-    "imageUrl": "https://dummyimage.com/250x250/0bc4ed/fff",
+    "imageUrl": "https://picsum.photos/200/100/",
     "stock": 627
   }, {
     "id": 3,
@@ -20,7 +20,7 @@ const products = [{
     "description": "Forcib correct defor NEC",
     "price": 11766.68,
     "category": "Windler, Sporer and Lakin",
-    "imageUrl": "https://dummyimage.com/250x250/0747f6/fff",
+    "imageUrl": "https://picsum.photos/200/100/",
     "stock": 724
   }, {
     "id": 4,
@@ -28,7 +28,7 @@ const products = [{
     "description": "Implant CCM,total system",
     "price": 68446.59,
     "category": "Kovacek LLC",
-    "imageUrl": "https://dummyimage.com/250x250/#4f52d/fff",
+    "imageUrl": "https://picsum.photos/200/100/",
     "stock": 665
   }, {
     "id": 5,
@@ -36,7 +36,7 @@ const products = [{
     "description": "C.A.T. scan of kidney",
     "price": 87747.92,
     "category": "Schuppe LLC",
-    "imageUrl": "https://dummyimage.com/250x250/02685e/fff",
+    "imageUrl": "https://picsum.photos/200/100/",
     "stock": 906
   }, {
     "id": 6,
@@ -44,7 +44,7 @@ const products = [{
     "description": "Replac m/s immob dev NEC",
     "price": 55139.45,
     "category": "Jones Inc",
-    "imageUrl": "https://dummyimage.com/250x250/83ee11/fff",
+    "imageUrl": "https://picsum.photos/200/100/",
     "stock": 907
   }, {
     "id": 7,
@@ -52,7 +52,7 @@ const products = [{
     "description": "Insertion of iud",
     "price": 68505.57,
     "category": "Denesik Group",
-    "imageUrl": "https://dummyimage.com/250x250/cba9d9/fff",
+    "imageUrl": "https://picsum.photos/200/100/",
     "stock": 47
   }, {
     "id": 8,
@@ -60,7 +60,7 @@ const products = [{
     "description": "Ambu cardiac monitoring",
     "price": 73641.91,
     "category": "Hilpert, Little and Wuckert",
-    "imageUrl": "https://dummyimage.com/250x250/004f0b/fff",
+    "imageUrl": "https://picsum.photos/200/100/",
     "stock": 467
   }, {
     "id": 9,
@@ -68,7 +68,7 @@ const products = [{
     "description": "Electrosurg lid epilat",
     "price": 70494.28,
     "category": "Stark-Sauer",
-    "imageUrl": "https://dummyimage.com/250x250/a27f98/fff",
+    "imageUrl": "https://picsum.photos/200/100/",
     "stock": 511
   }, {
     "id": 10,
@@ -76,7 +76,7 @@ const products = [{
     "description": "Lymphatic system scan",
     "price": 20298.79,
     "category": "Pacocha-Runte",
-    "imageUrl": "https://dummyimage.com/250x250/0e0879/fff",
+    "imageUrl": "https://picsum.photos/200/100/",
     "stock": 104
   }, {
     "id": 11,
@@ -84,7 +84,7 @@ const products = [{
     "description": "Shoulder synovectomy",
     "price": 8164.35,
     "category": "Ledner-Considine",
-    "imageUrl": "https://dummyimage.com/250x250/c0e02a/fff",
+    "imageUrl": "https://picsum.photos/200/100/",
     "stock": 867
   }, {
     "id": 12,
@@ -92,7 +92,7 @@ const products = [{
     "description": "Reopen recent lap site",
     "price": 1055.08,
     "category": "Crist-Olson",
-    "imageUrl": "https://dummyimage.com/250x250/d07d50/fff",
+    "imageUrl": "https://picsum.photos/200/100/",
     "stock": 524
   }
 ]
