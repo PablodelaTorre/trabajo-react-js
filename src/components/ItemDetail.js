fca410503e3ff18aa201bc3be024bec86a16da88
Pablo <delatorre.pablodaniel@gmail.com>
2022-03-12 20:47:27 -0300

    ItemDetail terminado falta pulirlo

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -7,9 +7,9 @@ const ItemDetail = ({item}) => {
         <article className='contenedor-detalle'>
             <img src={item.image} alt="..." className='imagen-detalle'/>
             <h1 className=''>{item.title}</h1>
-            <p className=''>{item.price} $</p>
-            <p className="fs-6 d-flex justify-content-center">{item.description}</p> 
-            <ItemCount stock={6} initial={1}/>
+            <p className='fs-4 text-success precio-detalle'>{item.price} $</p>
+            <p className="fs-6 d-flex justify-content-center descripcion-detalle">{item.description}</p> 
+            <div className="contador-stock"><ItemCount stock={6} initial={1}/></div>
         </article>
     )
 }
