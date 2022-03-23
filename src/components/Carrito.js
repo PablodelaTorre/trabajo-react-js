64844260568778901b9c3cfddfa9ea63fdc98ad0
Pablo <delatorre.pablodaniel@gmail.com>
2022-03-23 19:17:23 -0300

    carrito-estilizado

--- src/components/Carrito.js
+++ src/components/Carrito.js
@@ -7,22 +7,20 @@ const Carrito = () => {
     const {carrito, removeItem} = useContext(contexto)
     
     return (
-        <div className='d-flex container justify-content-center'>
-            
-            <div className='row my-4'>
-            {carrito.map(producto => (
-                <div key={producto.id} className="contenedor-carrito my-4">
-                    <img src={producto.image} alt=""/>
-                    <p className='d-flex justify-content-center titulo-carrito fw-bold'>{producto.title}</p>
-                    <p className='d-flex justify-content-center precio-carrito fs-4 fw-bold'>{producto.price + "$"}</p>
-                    <p className='d-flex justify-content-center cantidad-carrito fs-4 text-info'>Cantidad: {producto.quantity}</p>
-                    <p className='d-flex justify-content-center text-success fs-3 total-parcial-carrito'>Total parcial: {producto.quantity * producto.price + "$"}</p>
-                    <button onClick={() => removeItem(producto.id)} className='btn, btn-danger eliminar-producto-carrito'>Eliminar Producto</button>
-                </div>
-            ))}
+        
+            <div className='render-carrito'>
+                {carrito.map(producto => (
+                    <div key={producto.id} className="contenedor-carrito my-4">
+                        <img src={producto.image} alt=""/>
+                        <p className='d-flex justify-content-center titulo-carrito fw-bold'>{producto.title}</p>
+                        <p className='d-flex justify-content-center precio-carrito fs-4 fw-bold'>{producto.price + "$"}</p>
+                        <p className='d-flex justify-content-center cantidad-carrito fs-4 text-info'>Cantidad: {producto.quantity}</p>
+                        <p className='d-flex justify-content-center text-success fs-3 total-parcial-carrito'>Total parcial: {producto.quantity * producto.price + "$"}</p>
+                        <button onClick={() => removeItem(producto.id)} className='btn, btn-danger eliminar-producto-carrito'>Eliminar Producto</button>
+                    </div>
+                ))}
             </div>
             
-        </div>
     )
 }      
 
