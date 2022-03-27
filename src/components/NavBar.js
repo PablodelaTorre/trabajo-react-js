de2431e9fece46fc1eab978ae7187b5aeba6b43f
Pablo <delatorre.pablodaniel@gmail.com>
2022-03-26 23:50:36 -0300

    numero en navbar desaparece si la cantidad de productos es 0

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,20 +8,32 @@ const NavBar = () => {
     
     const {calcCantidad} = useContext(contexto)
     
-    return (
-    <>
-        
-        <nav className='d-flex 100vw justify-content-between align-items-center bgNav'>
-            <Link to="/" className='text-decoration-none link-danger mx-1'><h1 className='fs-2'>Ecommerce</h1></Link>
-            <ul className='d-flex mx-4 align-items-center list-unstyled paddingNone marginNone'>
-                <li className='mx-3 linkNav'><Link className='text-decoration-none linkNav' to="/category/men's clothing">Ropa</Link></li>
-                <li className='mx-3 linkNav'><Link className='text-decoration-none linkNav' to="/category/jewelery">Joyería</Link></li>
-                <li className='mx-3 linkNav'><Link className='text-decoration-none linkNav' to="/category/electronics">Electrónicos</Link></li>
-                <li className='mx-3'><Link to="/carrito" className='text-decoration-none link-danger'><CarWidget/>{calcCantidad()}</Link></li>
-            </ul>
-        </nav>
-    </>
-    )
+
+    if(calcCantidad()==0){
+        return (
+            <nav className='d-flex 100vw justify-content-between align-items-center bgNav'>
+                <Link to="/" className='text-decoration-none link-danger mx-1'><h1 className='fs-2'>Ecommerce</h1></Link>
+                <ul className='d-flex mx-4 align-items-center list-unstyled paddingNone marginNone'>
+                    <li className='mx-3 linkNav'><Link className='text-decoration-none linkNav' to="/category/men's clothing">Ropa</Link></li>
+                    <li className='mx-3 linkNav'><Link className='text-decoration-none linkNav' to="/category/jewelery">Joyería</Link></li>
+                    <li className='mx-3 linkNav'><Link className='text-decoration-none linkNav' to="/category/electronics">Electrónicos</Link></li>
+                    <li className='mx-3'><Link to="/carrito" className='text-decoration-none link-danger'><CarWidget/></Link></li>
+                </ul>
+            </nav>
+        )
+    } else {
+        return (
+            <nav className='d-flex 100vw justify-content-between align-items-center bgNav'>
+                <Link to="/" className='text-decoration-none link-danger mx-1'><h1 className='fs-2'>Ecommerce</h1></Link>
+                <ul className='d-flex mx-4 align-items-center list-unstyled paddingNone marginNone'>
+                    <li className='mx-3 linkNav'><Link className='text-decoration-none linkNav' to="/category/men's clothing">Ropa</Link></li>
+                    <li className='mx-3 linkNav'><Link className='text-decoration-none linkNav' to="/category/jewelery">Joyería</Link></li>
+                    <li className='mx-3 linkNav'><Link className='text-decoration-none linkNav' to="/category/electronics">Electrónicos</Link></li>
+                    <li className='mx-3'><Link to="/carrito" className='text-decoration-none link-danger'><CarWidget/>{calcCantidad()}</Link></li>
+                </ul>
+            </nav>
+        )
+    }
 }
 
 export default NavBar
